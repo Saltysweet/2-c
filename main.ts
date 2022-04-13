ab02f2b5f2598a10d4258d246dc0b5613eae0675
Saltysweet <100366224+Saltysweet@users.noreply.github.com>
2022-04-13 11:06:39 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 100; index++) {
+        number += 1
+        sum = number + sum
+    }
+    basic.showNumber(sum)
+})
+let sum = 0
+let number = 0
+number = 0
+sum = 0
